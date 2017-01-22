2a2e65196c8052969279ff016734e34f54408331
Denis Gagarsky <dega.uhta@gmail.com>
2017-01-22 06:03:41 +0300

    Chat fix

--- chapter_003/src/main/java/ru/job4j/Abuse.java
+++ chapter_003/src/main/java/ru/job4j/Abuse.java
@@ -2,10 +2,11 @@ package ru.job4j;
 
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.IOException;
-import java.util.Arrays;
 
 /**
  * Abuse class.
@@ -22,34 +23,13 @@ public class Abuse {
      * @param abuse array of abuse string.
      */
     public void dropAbuses(InputStream in, OutputStream out, String[] abuse) {
-        try (BufferedInputStream bis = new BufferedInputStream(in);
-             BufferedOutputStream bof = new BufferedOutputStream(out)) {
-            int oneByte;
-            int[] abuseCharFound = new int[abuse.length];
-            byte[] buffer = new byte[32];
-            int counter = 0;
-            while ((oneByte = bis.read()) != -1) {
-                if (counter > buffer.length) {
-                    buffer = Arrays.copyOf(buffer, buffer.length * 2);
-                }
-                buffer[counter++] = (byte) oneByte;
-                for (int i = 0; i < abuse.length; i++) {
-                    if (abuse[i].charAt(abuseCharFound[i]) == oneByte) {
-                        abuseCharFound[i]++;
-                        if (abuseCharFound[i] == abuse[i].length()) {
-                            abuseCharFound[i] = 0;
-                            for (int j = counter - abuse[i].length(); j < counter; j++) {
-                                buffer[j] = 0;
-                            }
-                            counter = counter - abuse[i].length();
-                            break;
-                        }
-                    } else {
-                        abuseCharFound[i] = 0;
-                    }
-                }
+        try (BufferedReader bufReader = new BufferedReader(new InputStreamReader(in));
+             BufferedWriter bufWriter = new BufferedWriter(new OutputStreamWriter(out))) {
+            String line;
+            while ((line = bufReader.readLine()) != null) {
+                line = line.replaceAll("one|two", "");
+                bufWriter.write(line);
             }
-            out.write(buffer, 0, counter);
         } catch (IOException e) {
             e.printStackTrace();
         }
